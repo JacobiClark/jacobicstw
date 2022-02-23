a736f4fccd16f4d5797054c6f81cb86c02a91ce1
Jacob Clark <jacobcs@zoho.com>
2022-02-22 22:05:18 -0800

    added showNav function

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { useTheme } from "next-themes";
 import { HiOutlineSun } from "react-icons/hi";
 import { FaRegMoon } from "react-icons/fa";
+import OYButton from "../components/buttons/OYButton";
 
 export default function Navbar() {
   const { theme, setTheme } = useTheme();
+  const [showNav, setShowNav] = useState(false);
 
   const switchTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
@@ -12,14 +14,8 @@ export default function Navbar() {
   };
 
   return (
-    <div className="flex justify-between w-full p-3">
-      <button
-        onClick={switchTheme}
-        type="button"
-        className="p-2 pl-5 pr-5 text-lg text-green-500 bg-transparent border-2 border-green-300 rounded-lg hover:bg-green-500 hover:text-gray-100 dark:text-white"
-      >
-        asdf
-      </button>
+    <div className="flex items-center justify-between w-full px-16 py-4">
+      {showNav && <h1 className="text-xl">{"{JC}"}</h1>}
       <button
         onClick={switchTheme}
         type="button"
@@ -27,6 +23,8 @@ export default function Navbar() {
       >
         <h3>{theme === "light" ? <FaRegMoon /> : <HiOutlineSun />}</h3>
       </button>
+      <button onClick={() => setShowNav(!showNav)}>asdf</button>
+      <OYButton buttonText="Resume" onClick={switchTheme} />
     </div>
   );
 }
